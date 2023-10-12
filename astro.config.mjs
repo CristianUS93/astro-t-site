66428edd33a87e7ab5b4734a21fd2d780eab4979
cristianUS <c.u.s21193@gmail.com>
2023-10-12 12:20:41 -0500

    fin tutorial astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://cheerful-raindrop-90c175.netlify.app"
+  site: "https://cheerful-raindrop-90c175.netlify.app",
+  integrations: [preact()]
 });
